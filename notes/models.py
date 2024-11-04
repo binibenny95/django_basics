b34c2e98cacc3d362c4e91790b89b769bd5f36d2
binibenny95 <binibenny95@gmail.com>
2024-11-04 12:17:48 +0100

    tasks

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 # Create your models here.
@@ -6,10 +7,20 @@ from datetime import datetime
 from django.db import models
 from django.utils import timezone
 
-class Note(models.Model):
-    title = models.CharField(verbose_name='Note Title', max_length=255)
-    content = models.TextField(verbose_name='Note Content', max_length=255)
-    date = models.DateField(verbose_name='Note Date', auto_now_add=True)
+
+class Category(models.Model):
+    title = models.CharField(verbose_name='Category Title', max_length=255)
 
     def __str__(self):
         return self.title
+
+class Note(models.Model):
+    title = models.CharField(verbose_name='Note Title', max_length=255)
+    text = models.TextField(verbose_name='Note Text', max_length=5000)
+    reminder = models.TextField(verbose_name='Note Reminder', max_length=5000)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
+
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,81 @@
+from audioop import reverse
+from lib2to3.fixes.fix_input import context
+from pyexpat.errors import messages
+from unicodedata import category
+
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Category, Note
+
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-     return HttpResponse('hello from the note app')
+    # note_list =[
+    #      Note(title='lesson 32', text='pending', reminder='done by today'),
+    #      Note(title='lesson 33', text='pending', reminder='done by today'),
+    #      Note(title='lesson 34', text='pending', reminder='done by today'),
+    #      Note(title='lesson 35', text='pending', reminder='done by today'),
+    # ]
+    # Note.objects.bulk_create(note_list)
+    notes_data = Note.objects.all()
+    return render(request, 'notes/index.html', {'notes_data': notes_data})
 
-def notes(request: HttpRequest) -> HttpResponse:
-     content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-     return  render(request,'notes/index.html',{'content':content})
+
+def add(request: HttpRequest) -> HttpResponse:
+    return render(request, 'notes/add.html')
+
+# def list_notes(request: HttpRequest) -> HttpResponse:
+#    notes = Note.objects.all()
+#    return render(request, 'notes/list.html', {'notes': notes})
+def list_notes(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        note_title = request.POST['title']
+        note_text = request.POST['text']
+        note_reminder = request.POST['reminder']
+        category_title = request.POST['category']
+        #create category first
+        category_title = Category.objects.create(title=category_title)
+        # create a new note using Note model
+        note = Note(title=note_title, text=note_text, reminder=note_reminder, category=category_title)
+
+        note.save()
+        ##collect note from db and list them on list.html
+        notes = Note.objects.all()
+        return render(request, 'notes/list.html', {'notes': notes})
+
+    else:
+        context = 'something went wrong ! please add again!'
+        return render(request, 'notes/add.html', context)
+
+def edit_note(request: HttpRequest, id: int) -> HttpResponse:
+    note = Note.objects.get(pk=id)
+
+    if request.method == 'POST':
+        note_title = request.POST['title']
+        note_text = request.POST['text']
+        note_reminder = request.POST['reminder']
+        category_title = request.POST['category']
+
+        category_title = Category.objects.create(title=category_title)
+
+        # update  a new note using Note model
+        note.title = note_title
+        note.text = note_text
+        note.reminder = note_reminder
+        note.category = category_title
+        note.save()
+        notes = Note.objects.all()
+        return render(request, 'notes/list.html', {'notes': notes})
+    else:
+        context = {'note': note}
+        return render(request, 'notes/edit.html', context)
+
+def delete_note(request: HttpRequest, id:int) -> HttpResponse:
+    note = Note.objects.get(pk=id)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note:add')
+    return HttpResponse('Deleted successfully')
+
+
+
